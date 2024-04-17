ed2f1bc4f6813f6035866dd7f8ace0b003239d0f
Juhana S <juhana.suhonen@codemate.com>
2024-04-17 12:24:57 +0300

    Add logging setup

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/juhanas/golang-training-advanced/internal/handlers"
 )
 
+var appEnv = os.Getenv("appEnv")
+
 func main() {
+	setupLogger()
+
 	router := setupRouter()
 
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
@@ -15,12 +22,34 @@ func main() {
 func setupRouter() *gin.Engine {
 	// Init Gin router with default values
 	router := gin.Default()
+
+	// Logger middleware logs to os.StdOut
+	router.Use(gin.Logger())
+
 	// Recovery middleware recovers from any panics and writes a 500 if there was one.
 	router.Use(gin.Recovery())
 
 	initRoutes(router)
 
 	return router
+}
+
+func setupLogger() {
+	logLevel := slog.LevelInfo
+	opts := &slog.HandlerOptions{
+		// Include log source (file & line number) if using debug
+		AddSource: logLevel == slog.LevelDebug,
+		// Set desired logging level
+		Level: logLevel,
+	}
+	var handler slog.Handler = slog.NewTextHandler(os.Stdout, opts)
+	if appEnv == "production" {
+		// Set logging output to happen as JSON
+		// This affects the whole project
+		handler = slog.NewJSONHandler(os.Stdout, opts)
+	}
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
 }
 
 func initRoutes(router *gin.Engine) {
